7ac964f78551e26a8b77ef209e2782ad9fbf9cc5
Ahmed Malek <ahmedmalekvic@mail.ru>
2022-01-19 23:12:00 +0200

    Update our menu-item to navigate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+// Styles
 import './App.css';
 
 // Components
 import HomePage from './pages/homepage/homepage.component';
 
 const App = () => (
-	<div>
-		<HomePage/>
-	</div>
+  <div>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+    </Routes>
+  </div>
 );
-
 
 export default App;
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
-
+import { useNavigate, useLocation } from 'react-router-dom';
 import './menu-item.styles.scss';
 
-export const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+export const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  let history = useNavigate();
+  const { pathname } = useLocation();
+  return (
     <div
-      className='background-image'
-      style={{
-        backgroundImage: `url(${imageUrl})`,
-      }}
-    />
-    <div className='content'>
-      <h1 className='title'>{title}</h1>
-      <span className='subtitle'>Shop now</span>
+      className={`${size} menu-item`}
+      onClick={() => history(`${pathname}${linkUrl}`)}>
+      <div
+        className='background-image'
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className='content'>
+        <h1 className='title'>{title}</h1>
+        <span className='subtitle'>Shop now</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
