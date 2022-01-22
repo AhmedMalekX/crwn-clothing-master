ba8668c3776405cc6f1d00c2f0317ecd7aa6a0c3
Ahmed Malek <ahmedmalekvic@mail.ru>
2022-01-22 22:46:34 +0200

    Optimizing Production

--- src/redux/store.js
+++ src/redux/store.js
@@ -5,7 +5,11 @@ import { persistStore } from 'redux-persist';
 // Root reducder
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
